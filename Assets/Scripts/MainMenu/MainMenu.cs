a8caa81e8ba0cc0c7eaf84b84feac8f5f2cb7a05
K.P <kim.pham@campus.lmu.de>
2021-05-27 13:17:59 +0200

    Added MainMenu Buttons
    Settings Button
    Back Button
    Quit Button

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -11,18 +11,6 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-
-    public void GoToSettingsMenu()
-    {
-        SceneManager.LoadScene("SettingsMenu");
-    }
-
-    public void GoToMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
-
-
     public void QuitGame()
     {
     Application.Quit();
