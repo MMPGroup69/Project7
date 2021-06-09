63e7dda060cc282d3588d3dbea32cb50f937e8e8
aylin-lynai <77901667+aylin-lynai@users.noreply.github.com>
2021-06-10 00:30:36 +0200

    Animation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,13 +1,15 @@
-  using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D controller; //bezieht sich auf das Skript CharacterController2D in Unity
+    public Animator animator;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
+    bool crouch = false;
    
 
    
@@ -16,16 +18,32 @@ public class PlayerMovement : MonoBehaviour {
     {
         horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if(Input.GetButtonDown("Jump")){
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
 
+        if(Input.GetButtonDown("Crouch")){
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")){
+            crouch = false;
+        }
       
        
     }
+
+    public void OnLanding(){
+        animator.SetBool("IsJumping", false);
+    }
      
+     public void OnCrouching(bool isCrouching){
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
     void FixedUpdate (){
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
 }
