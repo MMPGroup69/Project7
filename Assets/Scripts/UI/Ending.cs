5f09aee6ad321792fb75f84fe750e26d3e7e89c8
K.P <kim.pham@campus.lmu.de>
2021-06-16 21:10:12 +0200

    added scripts

--- Assets/Scripts/UI/Ending.cs
+++ Assets/Scripts/UI/Ending.cs
@@ -3,26 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class Ending : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
-
     public void LoadMMenu()
     {
         SceneManager.LoadScene("MainMenu");
         Debug.Log("MMENU");
     }
 
-    public void LoadNextScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        Debug.Log("RETRY");
-    }
-
-    public void LoadLastScene()
+    public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         Debug.Log("RETRY");
