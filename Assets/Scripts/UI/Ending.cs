5fc5e99bcb5ffde5dc386b6a138be0482c49efd8
K.P <kim.pham@campus.lmu.de>
2021-06-25 01:15:21 +0200

    added GG script

--- Assets/Scripts/UI/Ending.cs
+++ Assets/Scripts/UI/Ending.cs
@@ -7,7 +7,7 @@ public class Ending : MonoBehaviour
 {
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);         
         }
@@ -16,10 +16,12 @@ public class Ending : MonoBehaviour
     public void LoadMMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        //Debug.Log("MMENU");
     }
 
     public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        //Debug.Log("RETRY");
     }
 }
