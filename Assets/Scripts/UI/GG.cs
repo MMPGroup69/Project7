d5333334c6a6c8a85898730b63a63bd526db3b76
K.P <kim.pham@campus.lmu.de>
2021-06-29 02:13:29 +0200

    added icon

--- Assets/Scripts/UI/GG.cs
+++ Assets/Scripts/UI/GG.cs
@@ -8,13 +8,13 @@ public class GG : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.anyKey)
         {
             SceneManager.LoadScene("MainMenu");
         }
